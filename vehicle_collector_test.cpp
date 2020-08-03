d8bf8d072fd0ed2b554135f057955c34fce431f2
sudheerreddymoole <68851023+sudheerreddymoole@users.noreply.github.com>
2020-08-03 16:41:42 +0530

    Update vehicle_collector_test.cpp

--- vehicle_collector_test.cpp
+++ vehicle_collector_test.cpp
@@ -1,4 +1,18 @@
 #include <gtest/gtest.h>
+#include <vehicle_collector.h>
+
+TEST(COLLECT,When_Vehicle_GetsAllInputs)
+{
+    telematics ReceivedData
+    {
+        .vehicle_id = 103,
+        .type = motor_temp,
+        .measurement = 30
+    };
+    inventory StoredData;
+    StoredData = GetTelematics(ReceivedData);
+    ASSERT(StoredData.vehicle_id,ReceivedData.vehicle_id);
+}
 
 
 
